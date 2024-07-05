b7631a781c5d2f25b536211893d0eccf4d46a17f
Nihey Takizawa <nihey.takizawa@gmail.com>
2024-07-05 16:07:48 -0300

    feat: add basic setup file for an empty linux installation

---
+++ setup.sh
@@ -0,0 +1,6 @@
+apt install unattended-upgrades htop build-essential docker.io docker-compose vim rsync tree
+fallocate -l 2G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
